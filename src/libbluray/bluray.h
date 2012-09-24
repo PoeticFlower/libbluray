b0ebe5fb90bad47b4b30fe1d2cdaec28a858e698
Hendrik Leppkes <h.leppkes@gmail.com>
2012-09-24 02:01:25 +0200

    Include clpi_data.h to make debugging easier.

--- src/libbluray/bluray.h
+++ src/libbluray/bluray.h
@@ -32,6 +32,7 @@ extern "C" {
  */
 
 #include <stdint.h>
+#include "bdnav/clpi_data.h"
 
 #define TITLES_ALL              0    /**< all titles. */
 #define TITLES_FILTER_DUP_TITLE 0x01 /**< remove duplicate titles. */
@@ -867,7 +868,6 @@ const struct meta_dl *bd_get_meta(BLURAY *bd);
 
 /* access to internal information */
 
-struct clpi_cl;
 /**
  *
  *  Get copy of clip information for requested playitem.
